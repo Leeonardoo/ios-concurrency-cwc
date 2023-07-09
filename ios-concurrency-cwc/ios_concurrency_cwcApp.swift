8b96ff29a4fa6b8820e773917606c8ea92db44aa
Leonardo de Oliveira <leeonardo.deo@gmail.com>
2023-07-09 19:00:01 -0300

    Initial Commit

---
+++ ios-concurrency-cwc/ios_concurrency_cwcApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_concurrency_cwcApp.swift
+//  ios-concurrency-cwc
+//
+//  Created by Leonardo de Oliveira on 09/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_concurrency_cwcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
